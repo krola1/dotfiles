de95559ae8743372a620ccb74fa1d9ab49c75de3
krola1 <kwl082024@gmail.com>
2025-10-02 16:24:31 +0200

    changed from standard nvim to this nixvim flake 'github:elythh/nixvim'

--- flake.nix
+++ flake.nix
@@ -6,46 +6,49 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
+    nixvim.url = "github:elythh/nixvim";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    treefmt-nix,
-    ...
-  } @ inputs: let
-    lib = nixpkgs.lib;
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      treefmt-nix,
+      ...
+    }@inputs:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    # tree-wrapper som kjører alejandra
-    tree = treefmt-nix.lib.mkWrapper pkgs {
-      projectRootFile = "flake.nix";
-      programs.alejandra.enable = true;
-      programs.prettier.enable = true;
-      programs.shfmt.enable = true;
-      programs.black.enable = true;
-    };
-  in {
-    nixosConfigurations = {
-      nixos = lib.nixosSystem {
-        inherit system;
-        modules = [./configuration.nix];
+      # tree-wrapper som kjører alejandra
+      tree = treefmt-nix.lib.mkWrapper pkgs {
+        projectRootFile = "flake.nix";
+        programs.alejandra.enable = true;
+        programs.prettier.enable = true;
+        programs.shfmt.enable = true;
+        programs.black.enable = true;
       };
-    };
-    homeConfigurations = {
-      bandit = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [./home];
-        extraSpecialArgs = {inherit inputs;};
+    in
+    {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
       };
-    };
+      homeConfigurations = {
+        bandit = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home ];
+          extraSpecialArgs = { inherit inputs; };
+        };
+      };
 
-    # + gjør at `nix fmt` kjører treefmt (alejandra)
-    formatter.${system} = tree;
-  };
+      # + gjør at `nix fmt` kjører treefmt (alejandra)
+      formatter.${system} = tree;
+    };
 }
