3a4f6d7fa88cfbe2ff166865d88c9ccbece68cd6
krola1 <kwl082024@gmail.com>
2025-10-02 07:58:38 +0200

    changed keybinds.nix to a combinationfile with .conf

--- home/modules/hyprland/keybinds.nix
+++ home/modules/hyprland/keybinds.nix
@@ -1,55 +1,20 @@
-{...}: {
-  wayland.windowManager.hyprland.settings = {
-    "$mainMod" = "SUPER";
+# This is my attempt for å combinations system, .nix loops through repeatable binds, config file for the remainder
+{lib, ...}: {
+  # Bruk din eksisterende conf-fil
+  wayland.windowManager.hyprland.extraConfig = lib.mkAfter ''
+    source = ~/.config/hypr/keybinds.conf
+  '';
 
-    bind = [
-      "$mainMod, F, fullscreen"
-      "$mainMod, W, exec, $terminal"
-      "$mainMod, Q, killactive"
-      "$mainMod, M, exit"
-      "$mainMod, E, exec, $fileManager"
-      "Alt, W, togglefloating"
-      "$mainMod, R, exec, $menu"
-      "$mainMod, P, pseudo"
-      "$mainMod, J, togglesplit"
-      "$mainMod, B, exec, $browser"
-      "$mainMod, left, movefocus, l"
-      "$mainMod, right, movefocus, r"
-      "$mainMod, up, movefocus, u"
-      "$mainMod, down, movefocus, d"
-      "$mainMod, A, exec, wofi --show drun"
+  #
 
-      "$mainMod, 1, workspace, 1"
-      "$mainMod, 2, workspace, 2"
-      "$mainMod, 3, workspace, 3"
-      "$mainMod, 4, workspace, 4"
-      "$mainMod, 5, workspace, 5"
-      "$mainMod, 6, workspace, 6"
-      "$mainMod, 7, workspace, 7"
-      "$mainMod, 8, workspace, 8"
-      "$mainMod, 9, workspace, 9"
-      "$mainMod, 0, workspace, 10"
-
-      "$mainMod SHIFT, 1, movetoworkspace, 1"
-      "$mainMod SHIFT, 2, movetoworkspace, 2"
-      "$mainMod SHIFT, 3, movetoworkspace, 3"
-      "$mainMod SHIFT, 4, movetoworkspace, 4"
-      "$mainMod SHIFT, 5, movetoworkspace, 5"
-      "$mainMod SHIFT, 6, movetoworkspace, 6"
-      "$mainMod SHIFT, 7, movetoworkspace, 7"
-      "$mainMod SHIFT, 8, movetoworkspace, 8"
-      "$mainMod SHIFT, 9, movetoworkspace, 9"
-      "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-      "$mainMod, S, togglespecialworkspace, magic"
-      "$mainMod SHIFT, S, movetoworkspace, special:magic"
-      "$mainMod, mouse_down, workspace, e+1"
-      "$mainMod, mouse_up, workspace, e-1"
-    ];
-
-    bindm = [
-      "$mainMod, mouse:272, movewindow"
-      "$mainMod, mouse:273, resizewindow"
-    ];
-  };
+  wayland.windowManager.hyprland.settings.bind =
+    ["SUPER, RETURN, exec, kitty"]
+    #one map creates moves to workspace another displays it. pas through keys 1-10
+    ++ map (n: "SUPER, ${toString n}, workspace, ${toString n}") (lib.range 1 9)
+    ++ map (m: "SUPER SHIFT, ${toString m}, movetoworkspace, ${toString m}") (lib.range 1 9);
+  # Slink til .config
+  home.file.".config/hypr/keybinds.conf".source = ./keybinds.conf;
 }
+#bind = $mainMod SHIFT, 6, movetoworkspace, 6
+#bind = $mainMod SHIFT, 7, movetoworkspace, 7
+
